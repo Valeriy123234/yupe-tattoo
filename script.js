86f062aa3ae7baee8006292a64ccdeb45076a686
Valeriy123234 <warriors.wizard.2002@gmail.com>
2026-04-05 16:27:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        const loader = document.getElementById('loader');
+        const content = document.getElementById('main-content');
+        
+        loader.style.opacity = '0';
+        setTimeout(() => {
+            loader.style.display = 'none';
+            content.classList.remove('hidden');
+            content.classList.add('visible');
+        }, 1000); // час для плавного зникнення
+    }, 4000); 
+});
+
+function navTo(url) {
+    document.body.style.transition = '0.5s';
+    document.body.style.opacity = '0';
+    setTimeout(() => { window.location.href = url; }, 500);
+}
 
